72b9dac5e83120fa9f1a9724e6c75840a7120502
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-23 20:13:52 +0900

    [#10] canvas, svg 예시 추가

--- packages/react-pdf/storybook/svg.stories.tsx
+++ packages/react-pdf/storybook/svg.stories.tsx
@@ -1,19 +1,20 @@
 import React, {useEffect, useState} from 'react'
 
 import {getPdfDocument} from '../dist/cjs'
+import * as pdfjs from '../dist/cjs/pdf'
 
 import type {PDFDocumentProxy} from '../src/pdfjs-dist/types/pdfjs'
 
 const meta = {
     title: 'React PDF',
-    component: 기본_Canvas,
+    component: 기본_SVG,
 }
 export default meta
 
 const PDF_URL =
     'https://fs.pstatic.net/contents/resource/loan/personal-compare/required/0/1668583261818/gi_creditLoanNF_02.pdf'
 
-export function 기본_Canvas() {
+export function 기본_SVG() {
     const [pdfInfo, setPdfInfo] = useState<PDFDocumentProxy | undefined>()
 
     useEffect(() => {
@@ -31,7 +32,7 @@ export function 기본_Canvas() {
         <>
             {Array.from({length: pdfInfo.numPages}).map((_, index) => {
                 return (
-                    <canvas
+                    <div
                         key={index}
                         ref={async (node) => {
                             if (!node) {
@@ -39,13 +40,14 @@ export function 기본_Canvas() {
                             }
                             const page = await pdfInfo.getPage(index + 1)
                             const viewport = page.getViewport({scale: 1, rotation: 0})
-                            const canvasContext = node.getContext('2d')
-                            if (!canvasContext) {
-                                return
-                            }
-                            node.width = viewport.width
-                            node.height = viewport.height
-                            page.render({canvasContext, viewport, renderInteractiveForms: false})
+                            const operatorList = await page.getOperatorList()
+
+                            node.setAttribute('width', `${viewport.width}px`)
+                            node.setAttribute('height', `${viewport.height}px`)
+
+                            const svgGfx = new pdfjs.SVGGraphics(page.commonObjs, page.objs)
+                            const svg = await svgGfx.getSVG(operatorList, viewport)
+                            node.appendChild(svg)
                         }}
                     />
                 )
