00e455649f748fbd7d7582cc519cecbb32f9adba
soohyeon-lee <soohyeon.lee@navercorp.com>
2024-04-23 19:22:53 +0900

    :fire: 버저닝 제거 및 빌드 오류 수정

--- apps/docs/sidebars.ts
+++ apps/docs/sidebars.ts
@@ -1,8 +1,7 @@
-import {readFileSync, readdirSync} from 'fs'
+import {readFileSync} from 'fs'
 import path from 'path'
 
 import {globSync} from 'glob'
-import semver from 'semver'
 
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs'
 
@@ -36,23 +35,8 @@ readmdMdPaths.forEach((readmdMdPath) => {
         })
         .filter((md) => !md.includes('CHANGELOG.md'))
         .map((mdPath) => `${packageName}/${mdPath}`.replace(/\.md$/, ''))
-        .filter((mdPath) => semver.valid(mdPath.split('/')[1]))
 
-    const versions = readdirSync(path.join(docsPath, packageName), {withFileTypes: true})
-        .filter((dirent) => {
-            return dirent.isDirectory() && semver.valid(dirent.name.split('/')[0])
-        })
-        .map((dirent) => dirent.name)
-        .sort((a, b) => (semver.gt(a, b) ? -1 : 1))
-
-    const versionCategoryItems = versions.map((version) => {
-        return {
-            [version]: globSync(`**/*.md`, {
-                cwd: path.join(docsPath, packageName, version),
-            }).map((fileName) => `${packageName}/${version}/${fileName.replace(/\.md$/, '')}`),
-        }
-    })
-    docsSidebar[name] = [...mdPaths, ...versionCategoryItems]
+    docsSidebar[name] = [...mdPaths]
 })
 
 const sidebars: SidebarsConfig = {
