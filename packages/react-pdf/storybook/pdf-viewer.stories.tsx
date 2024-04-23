b5b4de0741403eb7ede16a24c71fe3c4a2b2e13d
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-23 22:21:54 +0900

    [#13] pdf viewer 스토리북을 추가합니다.

---
+++ packages/react-pdf/storybook/pdf-viewer.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {PDFViewer} from '../dist/cjs'
+
+const meta = {
+    title: 'React PDF',
+    component: 기본_PdfViewer,
+}
+export default meta
+
+const PDF_URL =
+    'https://fs.pstatic.net/contents/resource/loan/personal-compare/required/0/1668583261818/gi_creditLoanNF_02.pdf'
+
+export function 기본_PdfViewer() {
+    return <PDFViewer pdfUrl={PDF_URL} />
+}
