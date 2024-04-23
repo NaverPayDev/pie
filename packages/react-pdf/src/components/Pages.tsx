acd663098ab09608ce5a7390c4f9bf65773245d0
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-23 23:03:32 +0900

    [#13] 리뷰 반영: 이상한 문법을 제거합니다

--- packages/react-pdf/src/components/Pages.tsx
+++ packages/react-pdf/src/components/Pages.tsx
@@ -1,4 +1,4 @@
-import {Fragment, memo, useCallback} from 'react'
+import {Fragment, PropsWithChildren, memo, useCallback} from 'react'
 
 import {usePdfContext} from '../contexts/pdf'
 import {PageCanvas} from './page/Canvas'
@@ -8,10 +8,10 @@ export interface PagesProps {
     renderMode?: 'canvas' | 'svg'
 }
 
-export const Pages = memo(function Pages({renderMode}: PagesProps) {
+export const Pages = memo(function Pages({renderMode, children}: PropsWithChildren<PagesProps>) {
     const {pdf} = usePdfContext()
 
-    const renderPdf = useCallback(() => {
+    const RenderPDF = useCallback(() => {
         return Array.from({length: pdf.numPages}).map((_, index) => {
             const pageNumber = index + 1
             return (
@@ -23,15 +23,20 @@ export const Pages = memo(function Pages({renderMode}: PagesProps) {
         })
     }, [pdf.numPages, renderMode])
 
-    const renderTextLayer = useCallback(() => {}, [])
+    const RenderTextLayer = useCallback(() => {
+        return null
+    }, [])
 
-    const renderAnnotationLayer = useCallback(() => {}, [])
+    const RenderAnnotationLayer = useCallback(() => {
+        return null
+    }, [])
 
     return (
         <>
-            {renderPdf()}
-            {renderTextLayer()}
-            {renderAnnotationLayer()}
+            <RenderPDF />
+            <RenderTextLayer />
+            <RenderAnnotationLayer />
+            {children}
         </>
     )
 })
