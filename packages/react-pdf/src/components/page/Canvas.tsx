82418e79a02562037fa323388847262fd897eaf8
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-23 22:15:29 +0900

    [#13] canvas, svg render를 구현합니다.

--- packages/react-pdf/src/components/page/Canvas.tsx
+++ packages/react-pdf/src/components/page/Canvas.tsx
@@ -1,5 +1,34 @@
-import {memo} from 'react'
+import {memo, useCallback} from 'react'
 
-export const PageCanvas = memo(function PageCanvas() {
-    return null
+import {usePdfContext} from '../../contexts/pdf'
+
+interface PageCanvasProps {
+    pageNumber: number
+}
+
+export const PageCanvas = memo(function PageCanvas({pageNumber}: PageCanvasProps) {
+    const {pdf} = usePdfContext()
+
+    const drawCanvas = useCallback(
+        async (canvas: HTMLCanvasElement | null) => {
+            if (!canvas) {
+                return
+            }
+
+            const canvasContext = canvas.getContext('2d')
+            if (!canvasContext) {
+                return
+            }
+
+            const page = await pdf.getPage(pageNumber)
+            const viewport = page.getViewport({scale: 1, rotation: 0})
+
+            canvas.width = viewport.width
+            canvas.height = viewport.height
+            page.render({canvasContext, viewport})
+        },
+        [pageNumber, pdf],
+    )
+
+    return <canvas ref={drawCanvas} />
 })
