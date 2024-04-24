e1c987a952c697fde49282f7aa198b55248a3662
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-24 18:13:58 +0900

    [#24] 스토리북에서 pdf link는 새창으로 열 수 있도록 수정합니다

--- packages/react-pdf/storybook/pdf-viewer.stories.tsx
+++ packages/react-pdf/storybook/pdf-viewer.stories.tsx
@@ -13,5 +13,5 @@ export default meta
 const PDF_URL = 'https://financial.pstatic.net/static/terms-policy/npay-usage/231025.pdf'
 
 export function 기본_PdfViewer() {
-    return <PDFViewer pdfUrl={PDF_URL} />
+    return <PDFViewer pdfUrl={PDF_URL} options={{externalLinkTarget: '_blank'}} />
 }
