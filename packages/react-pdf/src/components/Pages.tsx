aad950123cb7517acaf5a9c0391ffefc4a281ae0
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-24 14:44:40 +0900

    [#14] 한 페이지에서, 페이지 호출을 한번만 하도록 수정합니다.

--- packages/react-pdf/src/components/Pages.tsx
+++ packages/react-pdf/src/components/Pages.tsx
@@ -1,6 +1,10 @@
-import {Fragment, PropsWithChildren, memo, useCallback} from 'react'
+import {PropsWithChildren, memo, useState} from 'react'
 
 import {usePdfContext} from '../contexts/pdf'
+import {useIsomorphicLayoutEffect} from '../hooks/useIsomorphicLayoutEffect'
+import {PDFPageProxy} from '../pdfjs-dist/types/pdfjs'
+import {AnnotationLayer} from './layer/Annotation'
+import {TextLayer} from './layer/Text'
 import {PageCanvas} from './page/Canvas'
 import {PageSvg} from './page/Svg'
 
@@ -8,34 +12,42 @@ export interface PagesProps {
     renderMode?: 'canvas' | 'svg'
 }
 
-export const Pages = memo(function Pages({renderMode, children}: PropsWithChildren<PagesProps>) {
+export const Page = memo(function Page({renderMode, pageNumber}: PagesProps & {pageNumber: number}) {
     const {pdf} = usePdfContext()
+    const [page, setPage] = useState<PDFPageProxy | undefined>()
 
-    const RenderPDF = useCallback(() => {
-        return Array.from({length: pdf.numPages}).map((_, index) => {
-            const pageNumber = index + 1
-            return (
-                <Fragment key={index}>
-                    {renderMode === 'canvas' && <PageCanvas pageNumber={pageNumber} />}
-                    {renderMode === 'svg' && <PageSvg pageNumber={pageNumber} />}
-                </Fragment>
-            )
-        })
-    }, [pdf.numPages, renderMode])
-
-    const RenderTextLayer = useCallback(() => {
-        return null
+    useIsomorphicLayoutEffect(() => {
+        async function init() {
+            const newPage = await pdf.getPage(pageNumber)
+            setPage(newPage)
+        }
+        init()
     }, [])
 
-    const RenderAnnotationLayer = useCallback(() => {
+    if (!page) {
         return null
-    }, [])
+    }
 
     return (
         <>
-            <RenderPDF />
-            <RenderTextLayer />
-            <RenderAnnotationLayer />
+            {renderMode === 'canvas' && <PageCanvas page={page} />}
+            {renderMode === 'svg' && <PageSvg page={page} />}
+            <TextLayer page={page} />
+            <AnnotationLayer page={page} />
+        </>
+    )
+})
+
+export const Pages = memo(function Pages({renderMode, children}: PropsWithChildren<PagesProps>) {
+    const {pdf} = usePdfContext()
+
+    // TODO : 무한 스크롤
+
+    return (
+        <>
+            {Array.from({length: pdf.numPages}).map((_, index) => {
+                return <Page key={index} renderMode={renderMode} pageNumber={index} />
+            })}
             {children}
         </>
     )
