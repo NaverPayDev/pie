b2ca04357779414fa4b7b09098e151fbdaea559d
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-24 16:38:36 +0900

    [#14] classname 설치 및 scss 설정 추가

---
+++ packages/react-pdf/typings/scss.d.ts
@@ -0,0 +1,14 @@
+declare module '*.module.css' {
+    const classes: Readonly<Record<string, string>>
+    export default classes
+}
+
+declare module '*.module.sass' {
+    const classes: Readonly<Record<string, string>>
+    export default classes
+}
+
+declare module '*.module.scss' {
+    const classes: Readonly<Record<string, string>>
+    export default classes
+}
