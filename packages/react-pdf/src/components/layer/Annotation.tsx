b9b81f73b20c3c33803509e801ec20a78b217eae
oneweek-lee <oneweek.lee@navercorp.com>
2024-04-24 18:03:47 +0900

    [#24] annotation layer 를 구현합니다.

--- packages/react-pdf/src/components/layer/Annotation.tsx
+++ packages/react-pdf/src/components/layer/Annotation.tsx
@@ -1,15 +1,24 @@
-import {memo, useState} from 'react'
+import {memo, useCallback, useState} from 'react'
 
+import classNames from 'classnames/bind'
+
+import {usePdfContext} from '../../contexts/pdf'
 import {useIsomorphicLayoutEffect} from '../../hooks/useIsomorphicLayoutEffect'
-import {PDFAnnotations} from '../../pdfjs-dist/types/pdfjs'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as pdfjs from '../../pdfjs-dist/legacy/build/pdf'
+import styles from './Annotation.module.scss'
 
-import type {PDFPageProxy} from '../../pdfjs-dist/types/pdfjs'
+import type {PDFAnnotations, PDFPageProxy} from '../../pdfjs-dist/types/pdfjs'
+
+const cx = classNames.bind(styles)
 
 interface AnnotationLayerProps {
     page: PDFPageProxy
 }
 
 export const AnnotationLayer = memo(function AnnotationLayer({page}: AnnotationLayerProps) {
+    const {linkService} = usePdfContext()
     const [annotations, setAnnotations] = useState<PDFAnnotations | undefined>()
 
     useIsomorphicLayoutEffect(() => {
@@ -20,9 +29,43 @@ export const AnnotationLayer = memo(function AnnotationLayer({page}: AnnotationL
         init()
     }, [page])
 
+    const drawAnnotation = useCallback(
+        (element: HTMLDivElement | null) => {
+            if (!element) {
+                return
+            }
+            const viewport = page.getViewport({scale: 1}).clone({dontFlip: true})
+            const parameters = {annotations, div: element, linkService, page, renderInteractiveForms: false, viewport}
+
+            try {
+                pdfjs?.AnnotationLayer?.render(parameters)
+            } catch {}
+
+            const children = element.children
+
+            if (children.length > 0 && children?.[0]) {
+                const firstChildren = children[0] as HTMLElement
+                firstChildren.style.position = 'absolute'
+
+                const aTags = firstChildren.getElementsByTagName('a') as unknown as HTMLAnchorElement[]
+
+                if (aTags.length > 0) {
+                    for (const elem of aTags) {
+                        elem.style.position = 'absolute'
+                        elem.style.top = '0'
+                        elem.style.left = '0'
+                        elem.style.width = '100%'
+                        elem.style.height = '100%'
+                    }
+                }
+            }
+        },
+        [annotations, linkService, page],
+    )
+
     if (!annotations) {
         return null
     }
 
-    return null
+    return <div ref={drawAnnotation} className={cx('annotationLayer')} />
 })
