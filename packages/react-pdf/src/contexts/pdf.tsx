ea57bfa3bb9764abbf76e91c5c54b4cfe6d33d03
oneweek-lee <oneweek.lee@navercorp.com>
2024-05-16 21:48:24 +0900

    [#20] context에 width를 주입받을 수 있도록 수정합니다

--- packages/react-pdf/src/contexts/pdf.tsx
+++ packages/react-pdf/src/contexts/pdf.tsx
@@ -7,7 +7,8 @@ export interface PdfProviderContext {
     /**
      * pdf 렌더링 시 필요한 props
      */
-    renderMode?: 'canvas' | 'svg'
+    width?: number
+    renderMode?: 'canvas' | 'svg' | null
     lazyLoading?: boolean
     tokenize?: boolean
     externalLinkTarget?: '_self' | '_blank' | '_parent' | '_top'
