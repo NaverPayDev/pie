2628ca1f22bd10c7fe70a018c6cfa1375f0ebb2f
oneweek-lee <oneweek.lee@navercorp.com>
2024-05-16 21:47:23 +0900

    [#20] resize hook 구현

---
+++ packages/react-pdf/src/hooks/usePdfViewerPageWidth.ts
@@ -0,0 +1,44 @@
+import {useEffect, useMemo, useState} from 'react'
+
+import {debounce} from '../utils/debounce'
+
+import type {RefObject} from 'react'
+
+const MAX_PAGE_WIDTH = 568
+const PADDING_WIDTH = 14
+
+const calculatedPageWidth = (windowWidth: number | undefined) => {
+    if (!windowWidth) {
+        return windowWidth
+    }
+    const adjustedWidth = windowWidth - PADDING_WIDTH
+
+    return Math.min(adjustedWidth, MAX_PAGE_WIDTH)
+}
+
+const usePdfViewerPageWidth = (refElement: RefObject<HTMLDivElement>) => {
+    const [windowWidth, setWindowWidth] = useState<number>()
+
+    const getClientWidth = debounce(() => {
+        const clientWidth = () => refElement?.current?.clientWidth || window.innerWidth || 0
+        setWindowWidth(clientWidth())
+    }, 100)
+
+    useEffect(() => {
+        getClientWidth()
+        window.addEventListener('resize', getClientWidth)
+        return () => {
+            window.removeEventListener('resize', getClientWidth)
+        }
+    }, [getClientWidth, refElement, setWindowWidth])
+
+    return useMemo(
+        () => ({
+            getClientWidth,
+            width: calculatedPageWidth(windowWidth),
+        }),
+        [getClientWidth, windowWidth],
+    )
+}
+
+export default usePdfViewerPageWidth
