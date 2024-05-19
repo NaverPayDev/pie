a5a8fb2cabe933380aab89230fb846f6c524c94e
oneweek-lee <oneweek.lee@navercorp.com>
2024-05-19 15:16:38 +0900

    [#39] size limit 시 private 인 패키지 필터링

--- .size-limit.js
+++ .size-limit.js
@@ -3,21 +3,17 @@ const glob = require('glob')
 const fs = require('fs')
 
 const packageJsonList = glob
-    .globSync('**/package.json', {
-        cwd: path.join(process.cwd(), 'packages'),
-    })
-    .map((filePath) => {
+    .globSync('**/package.json', {cwd: path.join(process.cwd(), 'packages')})
+    .reduce((acc, filePath) => {
         const {name, main, private} = JSON.parse(
             fs.readFileSync(path.join(process.cwd(), 'packages', filePath), 'utf8'),
         )
-        const packageName = name.split('/')[1]
-        return {
-            name: packageName,
-            private,
-            path: `packages/${packageName}${main.slice(1)}`,
-            limit: '500 ms',
+        if (private) {
+            return acc
         }
-    })
-    .filter(({private}) => !private)
+        const packageName = name.split('/')[1]
+        acc.push({name: packageName, path: `packages/${packageName}${main.slice(1)}`, limit: '500 ms'})
+        return acc
+    }, [])
 
 module.exports = packageJsonList
