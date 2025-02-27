240d4728193b3653d3f553a74618646d2c93cb63
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-02-27 20:08:08 +0900

    누락된 폴리필 세팅 추가

--- packages/vanilla-store/vite.config.mjs
+++ packages/vanilla-store/vite.config.mjs
@@ -3,6 +3,7 @@ import {createViteConfig} from '@naverpay/pite'
 export default createViteConfig({
     cwd: '.',
     entry: ['./src/index.ts'],
+    ignoredPolyfills: ['esnext.json.parse'],
     options: {
         minify: false,
     },
