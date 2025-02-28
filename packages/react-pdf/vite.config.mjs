c544a9e41649a6eaf79d4162a340336757847e38
oneweek-lee <oneweek.lee@navercorp.com>
2025-02-28 18:26:03 +0900

    entry 객체로 넣기

--- packages/react-pdf/vite.config.mjs
+++ packages/react-pdf/vite.config.mjs
@@ -3,7 +3,10 @@ import {createViteConfig} from '@naverpay/pite'
 export default createViteConfig({
     cwd: '.',
     cssFileName: 'style.css',
-    entry: ['./src/index.ts'],
+    entry: {
+        index: './src/index.ts',
+        'pdf.worker': './node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
+    },
     ignoredPolyfills: ['es.array.push'],
     options: {
         minify: false,
