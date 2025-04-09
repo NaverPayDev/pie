16525fd025f89fee24ebf2df2903e6ff0ba82d7a
yceffort <yc.effort@navercorp.com>
2025-04-09 20:04:22 +0900

    [#136] 🔧 fix: update worker source path to use legacy build in getPdfDocument function

--- packages/react-pdf/src/index.ts
+++ packages/react-pdf/src/index.ts
@@ -1,16 +1,3 @@
-if (typeof Promise.withResolvers === 'undefined') {
-    Promise.withResolvers = <T>() => {
-        let resolve: (value: T | PromiseLike<T>) => void
-        let reject: (reason?: unknown) => void
-        // eslint-disable-next-line promise/param-names
-        const promise = new Promise<T>((res, rej) => {
-            resolve = res
-            reject = rej
-        })
-        return {promise, resolve: resolve!, reject: reject!}
-    }
-}
-
 export * from './utils/pdf'
 export * from './components/page/Canvas'
 export * from './components/layer/Annotation'
