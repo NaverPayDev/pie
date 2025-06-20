24ff7ef7af268f05c2cf5a762497bfaacba2fa86
Ohjaeyeop <ohbcj0831@gmail.com>
2025-06-20 15:27:03 +0900

    [#166] 테스트 코드 추가

--- packages/url-compressor/src/UrlCompressor.ts
+++ packages/url-compressor/src/UrlCompressor.ts
@@ -1,1 +1,11 @@
-export class UrlCompressor {}
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export class URLCompressor {
+    compress(urlObj: Record<string, string>) {
+        return ''
+    }
+
+    decompress(compressedUrl: string, paramKeys: string[]) {
+        return {}
+    }
+}
