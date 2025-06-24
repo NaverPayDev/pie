847fa6df47d39637fb6a0fdcf1e087ca91821fb2
오재엽 <39650591+Ohjaeyeop@users.noreply.github.com>
2025-06-24 14:15:20 +0900

    URL Param 압축 도구 개발 (#169)
    * [#166] 테스트 코드 추가
    
    * [#166] test 스크립트 추가
    
    * [#166] install vitest
    
    * [#166] 패키지 네이밍 변경
    
    * [#166] 테스트 코드 데이터 수정
    
    * [#166] 테스트 코드 추가
    
    * [#166] URL 파라미터 압축 도구 구현
    
    * [#166] 테스트 케이스 보강
    - 압축 결과가 더 크거나 유효하지 않은 파라미터만 별도 처리
    
    * [#166] 테스트 코드 수정
    
    * [#166] install @types/node
    
    * [#166] 패키지명 수정
    
    * [#166] base64 복원
    
    * [#166] 테스트 url 수정 & 파라미터 전체를 압축하도록 수정
    
    * [#166] get method 추가
    
    * [#166] URLSearchParams 사용하도록 변경
    
    * [#166] LRU cache 추가
    
    ---------
    
    Co-authored-by: jaeyeop.oh <jaeyeop.oh@navercorp.com>

---
+++ packages/url-param-compressor/src/URLParamCompressor.ts
@@ -0,0 +1,88 @@
+import {deflateSync, inflateSync} from 'fflate'
+
+import LRUCache from './utils/LRUCache'
+
+export class URLParamCompressor {
+    private paramsMap: LRUCache<Map<string, string>>
+
+    constructor() {
+        this.paramsMap = new LRUCache(100)
+    }
+
+    private stringToUint8Array(value: string) {
+        return new TextEncoder().encode(value)
+    }
+
+    private uint8ArrayToString(value: Uint8Array) {
+        return new TextDecoder().decode(value)
+    }
+
+    private uint8ArrayToBase64URL(uint8: Uint8Array) {
+        const makeUrlSafe = (base64: string) => base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+
+        if (typeof Buffer !== 'undefined') {
+            return makeUrlSafe(Buffer.from(uint8).toString('base64'))
+        }
+
+        let binary = ''
+        // eslint-disable-next-line @typescript-eslint/prefer-for-of
+        for (let i = 0; i < uint8.length; i++) {
+            binary += String.fromCharCode(uint8[i])
+        }
+        return makeUrlSafe(btoa(binary))
+    }
+
+    private base64URLToUint8Array(base64: string) {
+        const restoreBase64 = (urlSafe: string) => {
+            const base64URL = urlSafe.replace(/-/g, '+').replace(/_/g, '/')
+            return base64URL.length % 4 === 0 ? base64URL : base64URL + '='.repeat(4 - (base64URL.length % 4))
+        }
+
+        if (typeof Buffer !== 'undefined') {
+            return new Uint8Array(Buffer.from(restoreBase64(base64), 'base64'))
+        }
+
+        const binary = atob(restoreBase64(base64))
+        const len = binary.length
+        const uint8 = new Uint8Array(len)
+        for (let i = 0; i < len; i++) {
+            uint8[i] = binary.charCodeAt(i)
+        }
+        return uint8
+    }
+
+    compress(urlObj: Record<string, string>) {
+        const param = new URLSearchParams(urlObj).toString()
+
+        const compressed = this.uint8ArrayToBase64URL(deflateSync(this.stringToUint8Array(param)))
+
+        return param.length <= compressed.length ? param : compressed
+    }
+
+    decompress(compressedParams: string) {
+        try {
+            const decompressed = this.uint8ArrayToString(inflateSync(this.base64URLToUint8Array(compressedParams)))
+
+            const decompressedParams = new URLSearchParams(decompressed)
+
+            const result = Array.from(decompressedParams.keys()).reduce<Record<string, string>>((acc, key) => {
+                acc[key] = decompressedParams.get(key)!
+                return acc
+            }, {})
+
+            this.paramsMap.put(compressedParams, new Map(Object.entries(result)))
+
+            return result
+        } catch {
+            return {}
+        }
+    }
+
+    get(compressed: string, key: string) {
+        if (this.paramsMap.get(compressed) === undefined) {
+            this.decompress(compressed)
+        }
+
+        return this.paramsMap.get(compressed)?.get(key)
+    }
+}
