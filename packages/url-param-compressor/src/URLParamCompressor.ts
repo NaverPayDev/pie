8eb6cc8380e0aa38015305bf6d08ac8869a1a657
jaeyeop.oh <jaeyeop.oh@navercorp.com>
2025-07-11 10:26:57 +0900

    [#166] jsdoc 추가 / 리드미 수정

--- packages/url-param-compressor/src/URLParamCompressor.ts
+++ packages/url-param-compressor/src/URLParamCompressor.ts
@@ -62,7 +62,13 @@ export class URLParamCompressor {
         return uint8
     }
 
-    compress(urlObj: Record<string, string>) {
+    /**
+     * @description url 쿼리 파라미터를 압축하여 url-safe base64 인코딩한 결과를 리턴합니다.
+     *
+     * @param {object} urlObj - 쿼리 파라미터를 key-value 형태로 전달합니다. value는 반드시 인코딩되어 있어야 합니다.
+     * @returns {{result: string, isCompressed: boolean}} 압축 결과와 압축 여부를 리턴합니다. 압축 결과가 원본보다 용량이 크다면 압축하지 않고 원본 객체를 string으로 변환하여 리턴합니다.
+     */
+    compress(urlObj: Record<string, string>): {result: string; isCompressed: boolean} {
         const param = new URLSearchParams(urlObj).toString()
         const compressed = this.uint8ArrayToBase64URL(deflateSync(this.stringToUint8Array(param), this.deflateOptions))
         const paramLen = param.length
@@ -83,10 +89,15 @@ export class URLParamCompressor {
         return {result, isCompressed}
     }
 
-    decompress(compressedParams: string) {
+    /**
+     *
+     * @param {string} compressed - compressor로 압축된 결과를 전달합니다.
+     * @returns {object} - 원본 객체를 리턴합니다. 압축 해제에 실패했다면 null을 리턴합니다.
+     */
+    decompress(compressed: string): Record<string, string> | null {
         try {
             const decompressed = this.uint8ArrayToString(
-                inflateSync(this.base64URLToUint8Array(compressedParams), {dictionary: this.deflateOptions.dictionary}),
+                inflateSync(this.base64URLToUint8Array(compressed), {dictionary: this.deflateOptions.dictionary}),
             )
 
             const decompressedParams = new URLSearchParams(decompressed)
@@ -96,7 +107,7 @@ export class URLParamCompressor {
                 return acc
             }, {})
 
-            this.paramsMap.put(compressedParams, new Map(Object.entries(result)))
+            this.paramsMap.put(compressed, new Map(Object.entries(result)))
 
             return result
         } catch (error) {
@@ -109,6 +120,12 @@ export class URLParamCompressor {
         }
     }
 
+    /**
+     *
+     * @param {string} compressed - compressor로 압축된 결과를 전달합니다.
+     * @param {string} key - 추출하고 싶은 쿼리 파라미터의 key를 전달합니다.
+     * @returns
+     */
     get(compressed: string, key: string) {
         if (this.paramsMap.get(compressed) === undefined) {
             this.decompress(compressed)
