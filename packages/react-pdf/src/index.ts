e54ac244f91f2c43527ec7620e497c7377c5a5df
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-11-25 18:51:25 +0900

    [#207] Promise.withResolvers 추가

--- packages/react-pdf/src/index.ts
+++ packages/react-pdf/src/index.ts
@@ -1,3 +1,7 @@
+// pdfjs-dist에서 Promise.withResolvers를 사용하므로 polyfill 주입을 위해 호출
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+Promise.withResolvers
+
 export * from './utils/pdf'
 export * from './components/page/Canvas'
 export * from './components/layer/Annotation'
